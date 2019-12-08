0fd3b75263c5afc340403da3ad59f9648c18cdef
muhammadirfan1994 <58412741+muhammadirfan1994@users.noreply.github.com>
2019-12-08 12:48:32 +0600

    Update 2019_11_19_053125_create_shoe_table.php
    Link added

--- database/migrations/2019_11_19_053125_create_shoe_table.php
+++ database/migrations/2019_11_19_053125_create_shoe_table.php
@@ -18,6 +18,7 @@ class CreateShoeTable extends Migration
             $table->timestamps();
             $table->string('Brand');
               $table->float('price');
+                $table->string("Link');
         });
     }
 
